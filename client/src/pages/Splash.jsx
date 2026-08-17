7a97083dbab320b29cae784e30a85d8a50a8fc9a
ditim15 <ditim456@gmail.com>
2026-08-17 14:39:18 -0400

    Added basic contents to splash page

--- client/src/pages/Splash.jsx
+++ client/src/pages/Splash.jsx
@@ -1,5 +1,17 @@
+import { Link } from 'react-router-dom';
+import "./Splash.css";
+
 function Splash() {
-    return <h1>Splash Page</h1>;
+    return (
+        <div className="splash-container">
+            <h1>Todo App</h1>
+            <p>Organize your day, one task at a time.</p>
+            <div className="splash-links">
+                <Link to="/register">Get Started</Link>
+                <Link to="/login">Log In</Link>
+            </div>
+        </div>
+    );
 }
 
 export default Splash;
