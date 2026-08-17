6357bb5e2cacfeed444e8842fd6d3c06e9cb78a0
ditim15 <ditim456@gmail.com>
2026-08-17 19:47:35 -0400

    increased authLimiter max requests for testing

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,7 @@ import rateLimit from 'express-rate-limit';
 
 const authLimiter = rateLimit({
     windowMs: 15 * 60 * 1000,
-    max: 5,
+    max: 50,
     message: { message: "Too many requests, please try again later" }
 });
 
