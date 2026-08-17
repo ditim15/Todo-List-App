0e094af5c6f14ebc95dc6f41e9ad9471a4966f2f
ditim15 <ditim456@gmail.com>
2026-08-17 18:30:30 -0400

    Wrap dashboard in protected route component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './auth/AuthContext';
+import ProtectedRoute from './auth/ProtectedRoute';
 import Splash from './pages/Splash';
 import Login from './pages/Login';
 import Register from './pages/Register';
@@ -14,7 +15,11 @@ function App() {
                     <Route path="/" element={<Splash />} />
                     <Route path="/login" element={<Login />} />
                     <Route path="/register" element={<Register />} />
-                    <Route path="/dashboard" element={<Dashboard />} />
+                    <Route path="/dashboard" element={
+                        <ProtectedRoute>
+                            <Dashboard />
+                        </ProtectedRoute>
+                    }/>
                     <Route path="*" element={<NotFound />} />
                 </Routes>
             </BrowserRouter>
